2a73647983e8f52ebdea6c969a687c19c8799dd5
timurgordon <timurgordon@gmail.com>
2022-10-24 14:19:24 +0300

    hurrying

--- ticket.v
+++ ticket.v
@@ -1,6 +1,11 @@
-
+import vweb
 
 pub fn render_ticket(qr string) {
 	slug := qr.all_after_last('_')
 	$tmpl('./ticket.html')
+}
+
+pub fn (mut app App) index() vweb.Result {
+	qrs := 
+	return $vweb.html()
 }
