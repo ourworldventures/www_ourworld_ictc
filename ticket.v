2ba237e505df8946f9df0708d0220137317e8323
Jonathan Ouwerx <jonathanouwerx@me.com>
2022-10-24 14:55:19 +0300

    read images

--- ticket.v
+++ ticket.v
@@ -9,14 +9,15 @@ pub fn ticket_front(qr string) string {
 }
 
 pub fn run_before() {
-	qrs := ['test']
+
+	mut qr_paths := os.walk_ext('qrs', 'png')
 	mut html := ''
 	os.rm('index.html') or { panic(err) }
 	mut index_file := os.create('index.html') or { panic('Failed to create index.html file: $err') }
 	println('debug:${typeof(index_file)}')
 	println('debug: ${ticket_front('test')}')
 	index_file.write_string('<div>') or { panic('Failed to write <div> to index.html file: $err') }
-	for qr in qrs {
+	for qr in qr_paths {
 		index_file.write_string(ticket_front(qr)) or {
 			panic('Failed to write qr: $qr to index.html file: $err')
 		}
